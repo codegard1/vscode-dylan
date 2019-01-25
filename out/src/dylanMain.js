cdf31ea05c34a27d87bb208a217e8c7d9f0c63c1
Chris Odegard <c.odegard@gmail.com>
2019-01-24 22:23:17 -0500

    Implement sayHello as test

--- out/src/dylanMain.js
+++ out/src/dylanMain.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function activate(context) { }
+const vscode = require("vscode");
+function activate(context) {
+    const command = "vscode-dylan.sayHello";
+    const commandHandler = (name = 'world') => { console.log(`Hello, ${name}!`); };
+    context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+}
 exports.activate = activate;
 //# sourceMappingURL=dylanMain.js.map
--- out/src/dylanMain.ts
+++ out/src/dylanMain.ts
@@ -1,3 +1,11 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  const command = "vscode-dylan.sayHello";
+
+  const commandHandler = (name: string = 'world') => { console.log(`Hello, ${name}!`); }
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(command, commandHandler)
+  )
+}
