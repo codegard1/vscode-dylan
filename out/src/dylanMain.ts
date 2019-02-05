fba512662bb10b60932a75d6c7a603c9af7fc102
Chris Odegard <c.odegard@gmail.com>
2019-02-04 22:34:59 -0500

    remove redundant lines sqaush this!

--- out/src/dylanMain.ts
+++ out/src/dylanMain.ts
@@ -1,16 +1,9 @@
 import * as vscode from "vscode";
-import * as path from 'path';
 import { makeDylanProject } from './make-dylanproject';
 
 export function activate(context: vscode.ExtensionContext) {
-  const currentFileName: string = vscode.window.activeTextEditor.document.fileName;
-  const currentFolder: string = path.dirname(currentFileName);
-  // console.log('current file:', currentFileName);
-  // console.log('current folder:', currentFolder);
-
   const command = "vscode-dylan.buildProject";
   const commandHandler = makeDylanProject;
-
   context.subscriptions.push(
     vscode.commands.registerCommand(command, commandHandler)
   )
