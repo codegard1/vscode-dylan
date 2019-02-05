089519af700ea65cb444fd2867d0495dad6d5cb1
Chris Odegard <c.odegard@gmail.com>
2019-02-04 21:24:08 -0500

    add lines to get directory of current file the child process that invokes the build tool is starting in the wrong directory

--- out/src/dylanMain.js
+++ out/src/dylanMain.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
+const path = require("path");
+const make_dylanproject_1 = require("./make-dylanproject");
 function activate(context) {
+    const currentFileName = vscode.window.activeTextEditor.document.fileName;
+    const currentFolder = path.dirname(currentFileName);
+    // console.log('current file:', currentFileName);
+    // console.log('current folder:', currentFolder);
     const command = "vscode-dylan.buildProject";
-    const commandHandler = () => {
-    };
+    const commandHandler = make_dylanproject_1.makeDylanProject;
     context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
 }
 exports.activate = activate;
