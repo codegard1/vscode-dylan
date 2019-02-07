5755175e49981a78da1ceab16be42331b208dbcc
Chris Odegard <c.odegard@gmail.com>
2019-02-06 21:01:27 -0500

    change name of compiler to dylan-compiler for better cross-platform compatibility

--- out/src/make-dylanproject.ts
+++ out/src/make-dylanproject.ts
@@ -29,7 +29,7 @@ async function invokeBuildTool(lidFile: vscode.Uri[]): Promise<any> {
     const lidFileName: string = lidFile[0].fsPath;
 
     // The name / absolute location of the Dylan Compiler
-    const dylanCompiler: string = os.type() === "Windows_NT" ? 'dylan-compiler-with-tools.exe' : 'dylan-compiler-with-tools'
+    const dylanCompiler: string = os.type() === "Windows_NT" ? 'dylan-compiler.exe' : 'dylan-compiler'
     
     // Fail if the compiler executable can't be located
     if (!fs.existsSync(dylanCompiler)) { reject(`Could not find the compiler executable (${dylanCompiler}).\nIs is added to PATH?`); }
